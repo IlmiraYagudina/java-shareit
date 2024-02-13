e3823ab7e4636fbeffbdfe4fa54987975583b9c2
GutImozih73 <ilmirayagudina1996@gmail.com>
2024-02-13 22:09:39 +0300

    fix: delete excess exception

--- src/main/java/ru/practicum/shareit/exception/NotFoundStateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class NotFoundStateException extends RuntimeException {
-    public NotFoundStateException(String message) {
-        super(message);
-    }
-}
